f04c66d4a71f04eb72df34a1c95fc002e0ab23c5
Bryth <62302815+Brythzz@users.noreply.github.com>
2023-11-03 17:31:29 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+cfg: gen_llvm gen_cfg
+
+gen_llvm: something.c
+	clang -S -emit-llvm -Xclang -disable-O0-optnone something.c
+
+gen_cfg: something.ll
+	opt -p dot-cfg something.ll
+	dot -Tpdf .main.dot -o main.pdf
+	rm -f .*.dot
+
+pass:
+	@cd build && make && cd ..
+
+passll:
+	clang -fpass-plugin=`echo build/pass/FlattenCFGPass.*` something.c -emit-llvm -S -o something.ll
+
+clean:
+	rm -f .*.dot
+	rm -f *.ll
+	rm -f *.pdf
+
+.PHONY: pass clean
---
+++ pass/CFGFlatten.cpp
@@ -0,0 +1,144 @@
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/CFG.h"
+
+#define DEBUG_TYPE "flatten-cfg"
+STATISTIC(Flattened, "Number of functions flattened");
+
+using namespace llvm;
+
+/**
+* @brief Splits the entry block if it ends with a conditional branch
+* to make it unconditionally jump to the dispatcher
+*/
+static BasicBlock* splitEntryBlock(BasicBlock *entryBlock) {
+  Instruction *term = entryBlock->getTerminator();
+
+  if (BranchInst *br = dyn_cast<BranchInst>(term)) {
+    if (br->isConditional()) {
+      Value *cond = br->getCondition();
+
+      if (Instruction *inst = dyn_cast<Instruction>(cond)) { // may be a constant
+        entryBlock = entryBlock->splitBasicBlockBefore(inst, "newEntry");
+      }
+    }
+  }
+
+  else if (SwitchInst *sw = dyn_cast<SwitchInst>(term)) {
+    entryBlock = entryBlock->splitBasicBlockBefore(sw, "newEntry");
+  }
+
+  return entryBlock;
+}
+
+/**
+* @brief Iterates over basic blocks and dispatches them in a switch statement
+*/
+static bool flatten(Function &F) {
+  Type *i32_type = Type::getInt32Ty(F.getContext());
+
+  // Create switch variable on the stack
+  AllocaInst *switchVar = new AllocaInst(
+    Type::getInt32Ty(F.getContext()),
+    0,
+    "switchVar",
+    F.getEntryBlock().getFirstNonPHI()
+  );
+
+  // Create switch dispatcher block
+  BasicBlock *dispatcher = BasicBlock::Create(F.getContext(), "dispatcher", &F);
+  LoadInst *load = new LoadInst(i32_type, switchVar, "switchVar", dispatcher);
+  SwitchInst *sw = SwitchInst::Create(load, dispatcher, 0, dispatcher);
+
+  // Add all non-terminating basic blocks to the switch
+  BasicBlock *EntryBlock = splitEntryBlock(&F.getEntryBlock());
+
+  int idx = 0;
+  for (auto &B : F) { // make_early_inc_range not needed since no mutation is done
+    if (&B == EntryBlock || &B == dispatcher) continue; // Skip entry block
+
+    if (isa<ReturnInst>(B) || isa<UnreachableInst>(B)) continue; // No successors
+
+    // Create case variable
+    ConstantInt *swIdx = dyn_cast<ConstantInt>(ConstantInt::get(i32_type, idx));
+
+    sw->addCase(swIdx, &B);
+    idx++;
+  }
+
+  if (idx <= 1) return false; // Function too small to be flattened
+
+  // Update branches to set stateVariable conditionally
+  // Unconditionally branch to the dispatcher block
+  for (auto &B : F) {
+    ConstantInt *caseValue = sw->findCaseDest(&B);
+    if (!caseValue && &B != &F.getEntryBlock()) continue; // Skips blocks not added except entry
+
+    Instruction *term = B.getTerminator();
+
+    if (term->getNumSuccessors() == 1) {
+      BasicBlock *succ = term->getSuccessor(0);
+
+      ConstantInt *idx = sw->findCaseDest(succ);
+      new StoreInst(idx, switchVar, term);
+
+      term->eraseFromParent();
+      BranchInst::Create(dispatcher, &B);
+    }
+
+    if (term->getNumSuccessors() == 2) {
+      ConstantInt *idxTrue = sw->findCaseDest(term->getSuccessor(0));
+      ConstantInt *idxFalse = sw->findCaseDest(term->getSuccessor(1));
+
+      BranchInst *br = cast<BranchInst>(term);
+      SelectInst *sl = SelectInst::Create(br->getCondition(), idxTrue, idxFalse, "", term);
+      new StoreInst(sl, switchVar, term); // switchVar = cond ? idxTrue : idxFalse
+
+      term->eraseFromParent();
+      BranchInst::Create(dispatcher, &B);
+    }
+
+    // Do we need to handle switches?
+  }
+
+  return true;
+}
+
+
+namespace {
+
+struct FlattenCFGPass : public PassInfoMixin<FlattenCFGPass> {
+PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    for (auto &F : M.functions()) {
+      if (F.empty()) continue;
+
+      errs() << "Trying to flatten " << F.getName() << "!\n";
+
+      if (flatten(F)) Flattened++;
+    }
+
+    return Flattened == 0
+      ? PreservedAnalyses::all()
+      : PreservedAnalyses::none();
+  };
+};
+
+} // end namespace llvm
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = LLVM_PLUGIN_API_VERSION,
+        .PluginName = "CFGPass",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                    MPM.addPass(FlattenCFGPass());
+                });
+        }
+    };
+}
